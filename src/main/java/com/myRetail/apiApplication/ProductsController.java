722767803ffe48d4c162fa10b8d099221b8fad2e
Xa Vang <vangxbg@gmail.com>
2018-12-24 13:15:51 -0600

    adding various id to external api call

--- src/main/java/com/myRetail/apiApplication/ProductsController.java
+++ src/main/java/com/myRetail/apiApplication/ProductsController.java
@@ -24,12 +24,13 @@ public class ProductsController {
     	priceResource.currencyCode = price.getCurrencyCode();
     	priceResource.value = price.getValue();
     	
-    	ProductResource productResource = new ProductResource();
-    	productResource.id = price.getId();
-    	productResource.price = priceResource;
+    	String externalApiUri = "https://redsky.target.com/v2/pdp/tcin/"+id+"?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics";
+    	ExternalResource externalResource = restTemplate.getForObject(externalApiUri, ExternalResource.class);
     	
-    	ExternalResource externalResource = restTemplate.getForObject("https://redsky.target.com/v2/pdp/tcin/13860428?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics", ExternalResource.class);
-    	productResource.name = externalResource.product.item.product_description.title; 	
+    	ProductResource productResource = new ProductResource();
+    	productResource.id = id;
+    	productResource.name = externalResource.product.item.product_description.title; 
+    	productResource.price = priceResource;	
     	    	
     	return productResource;
     }

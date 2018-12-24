50f84b7ca691b823bc557da57586de358e37225d
Xa Vang <vangxbg@gmail.com>
2018-12-24 13:49:23 -0600

    added updateproduct

--- src/main/java/com/myRetail/apiApplication/ProductsController.java
+++ src/main/java/com/myRetail/apiApplication/ProductsController.java
@@ -3,6 +3,7 @@ import org.springframework.web.client.RestTemplate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,14 +25,32 @@ public class ProductsController {
     	priceResource.currencyCode = price.getCurrencyCode();
     	priceResource.value = price.getValue();
     	
-    	String externalApiUri = "https://redsky.target.com/v2/pdp/tcin/"+id+"?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics";
-    	ExternalResource externalResource = restTemplate.getForObject(externalApiUri, ExternalResource.class);
+    	ExternalResource externalResource = restTemplate.getForObject("https://redsky.target.com/v2/pdp/tcin/"+id+"?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics", ExternalResource.class);
     	
     	ProductResource productResource = new ProductResource();
     	productResource.id = id;
     	productResource.name = externalResource.product.item.product_description.title; 
     	productResource.price = priceResource;	
     	    	
+    	return productResource;
+    }
+    
+    @RequestMapping(method=RequestMethod.PUT, value="/products/{id}")
+    public ProductResource UpdateProduct(RestTemplate restTemplate, @PathVariable Long id, @RequestBody PriceResource priceResource) {
+    	
+    	Price price = repository.findById(id).get();
+    	
+    	price.currencyCode = priceResource.getCurrencyCode();
+    	price.value = priceResource.getValue();
+    	repository.save(price);
+    	
+    	ExternalResource externalResource = restTemplate.getForObject("https://redsky.target.com/v2/pdp/tcin/"+id+"?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics", ExternalResource.class);
+    	
+    	ProductResource productResource = new ProductResource();
+    	productResource.id = id;
+    	productResource.name = externalResource.product.item.product_description.title; 
+    	productResource.price = priceResource;	    	
+    	
     	return productResource;
     }
 }
